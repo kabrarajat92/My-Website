6b12b1bc8c3b5ebc50ce1826ba16de274020a792
Rajat Kabra <99293557+kabrarajat92@users.noreply.github.com>
2024-03-01 22:44:17 +0530

    Add files via upload

--- Rajat Kabra website/js/index-new.js
+++ Rajat Kabra website/js/index-new.js
@@ -1062,7 +1062,7 @@ function initContactForm() {
           $(this).parent().toggleClass('not-empty', text_val !== "");
       }).focusout();
   });
-
+  
 }
 
 /**
@@ -1078,7 +1078,7 @@ function initTimeZone() {
   const timeSpan = document.querySelector("#timeSpan");
 
   const optionsTime = {
-    timeZone: 'Europe/Amsterdam',
+    timeZone: 'Asia/Kolkata',
     timeZoneName: 'short',
     // year: 'numeric',
     // month: 'numeric',
